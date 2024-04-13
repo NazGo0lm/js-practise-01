ebf866dcc58bb521ff9cff3f2216cc9fbf186c55
Anna <iaanya27@gmail.com>
2024-04-13 11:43:59 +0300

    added task4

---
+++ js/task4.js
@@ -0,0 +1,30 @@
+/**
+ *? Напишіть через свіч пошук автора мови програмування.
+ *? Пишемо назву мови у шаблонному рядку отримує відповідь
+ *? мова та автор
+ *? PHP Расмус Лердорф
+ *? C# група інженерів компанії Microsoft під керівництвом Андерса Хейлсберга та Скотта Вільтаумота
+ *? Swift Кріс Латтнер
+ *? JS Брендан Ейх
+ *? Java Джеймс Гослінг
+ *? Python Гвідо ван Россум
+ */
+
+const result = prompt("Введіть мову програмування");
+switch (result) {
+    case "PHP": console.log("Расмус Лердорф");
+        break;
+    case "C#": console.log("група інженерів компанії Microsoft під керівництвом Андерса Хейлсберга та Скотта Вільтаумота");
+        break;
+    case "Swift": console.log("Кріс Латтнер");
+        break; 
+    case "JS": console.log("Брендан Ейх");
+        break; 
+    case "Java": console.log("Джеймс Гослінг");
+        break;
+    case "Python": console.log("Гвідо ван Россум");
+        break;
+    default: console.log("Not found");
+        break;
+}
+ 
